09ebb476c528de84c8bd559209e5618f4f735ae9
Lisandro Cuello <cuellolisandro8@gmail.com>
2022-02-15 01:06:46 -0300

    Add files via upload

---
+++ Store/Store/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Store.Views;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Store
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TBShow(object sender, RoutedEventArgs e)
+        {
+            GridContent.Opacity = 0.5;
+        }
+
+        private void TBHide(object sender, RoutedEventArgs e)
+        {
+            GridContent.Opacity = 1;
+        }
+
+        private void PreviewMouseLeftButtonDownBG(object sender, MouseButtonEventArgs e)
+        {
+            BtnShowHide.IsChecked = false;
+        }
+
+        private void Minimizar(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Cerrar(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Usuarios_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new Usuarios();
+        }
+    }
+}
